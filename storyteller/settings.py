e5dd86f82802bcee9eb72ea5431f1fe1581bad89
three-js-master <royal309978@gmail.com>
2020-11-03 12:27:07 +0530

    Deploy HotFix 2

--- storyteller/settings.py
+++ storyteller/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.getenv("DEBUG"))
+DEBUG = os.getenv("DEBUG") == "1"
 
 ALLOWED_HOSTS = ['story.pythonanywhere.com', '127.0.0.1']
 
